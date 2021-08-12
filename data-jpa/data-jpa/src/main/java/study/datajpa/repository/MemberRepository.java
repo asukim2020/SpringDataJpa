1d519982aab3c4171c0ea591487b88a79bfb9c9d
asukim2020 <63150458+asukim2020@users.noreply.github.com>
2021-08-12 17:55:18 +0900

    first commit

---
+++ data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package study.datajpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+//    @Query(name = "Member.findByUsername") 사용 안함
+    List<Member> findByUsername(@Param("username") String username);
+    
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+}
