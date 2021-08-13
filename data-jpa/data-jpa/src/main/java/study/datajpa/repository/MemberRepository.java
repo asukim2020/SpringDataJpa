28874ced4e63b450d25129447d85686d4ae77434
asukim2020 <63150458+asukim2020@users.noreply.github.com>
2021-08-13 10:13:41 +0900

    Query 값, DTO 조회하기

--- data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -13,9 +14,19 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
+    // @NamedQuery로 조회
 //    @Query(name = "Member.findByUsername") 사용 안함
     List<Member> findByUsername(@Param("username") String username);
-    
+
+    // 쿼리로 조회
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    // 인자 값 조회
+    @Query("select m.username from Member m")
+    List<String> findUserNameList();
+
+    // Dto 로 조회
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
