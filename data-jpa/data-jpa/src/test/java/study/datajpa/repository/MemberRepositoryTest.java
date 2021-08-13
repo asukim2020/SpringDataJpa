8815bd50203b553bd7c95560d9f7a32aa4412e8f
asukim2020 <63150458+asukim2020@users.noreply.github.com>
2021-08-13 14:07:31 +0900

    HInt & Lock

--- data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -264,4 +264,30 @@ public class MemberRepositoryTest {
 //            member.getTeam().getName();
         }
     }
+
+    @Test
+    public void queryHint() {
+        //given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+
+        //when
+//        Member findMember = memberRepository.findById(member1.getId()).get();
+        Member findMember = memberRepository.findReadOnlyByUsername(member1.getUsername());
+        findMember.setUsername("member2");
+
+        em.flush();
+    }
+
+    @Test
+    public void lock() {
+        //given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> lockByUsername = memberRepository.findLockByUsername(member1.getUsername());
+    }
 }
