90b84f70c90f2c52cb8d4a9984f7bf440620856d
asukim2020 <63150458+asukim2020@users.noreply.github.com>
2021-08-13 15:19:17 +0900

    Auditing - 엔티티에 공통되는 인자 별로 처리

--- data-jpa/data-jpa/src/main/java/study/datajpa/controller/HelloController.java
+++ data-jpa/data-jpa/src/main/java/study/datajpa/controller/HelloController.java
@@ -1,13 +1,35 @@
 package study.datajpa.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final MemberRepository memberRepository;
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("member1"));
+    }
 
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/member")
+    public List<Member> list() {
+        return memberRepository.findAll();
+    }
+
 }
--- data-jpa/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,14 +1,17 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +22,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -51,4 +57,27 @@ class MemberTest {
         }
     }
 
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member);
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();
+        
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+//        System.out.println("findMember.getCreateDate() = " + findMember.getCreateDate());
+//        System.out.println("findMember.getUpdateDate() = " + findMember.getUpdateDate());
+        System.out.println("findMember.getCreateDate() = " + findMember.getCreatedDate());
+        System.out.println("findMember.getUpdateDate() = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.getCreateDate() = " + findMember.getCreatedBy());
+        System.out.println("findMember.getUpdateDate() = " + findMember.getLastModifiedBy());
+    }
 }
